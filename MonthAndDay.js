93f19accf747762943dc8ab31ff3e1af098eee9b
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-01 18:05:16 +0530

    Take input from user day and month in between condition return true or false

---
+++ MonthAndDay.js
@@ -0,0 +1,14 @@
+//Whenever u use user input fisrt u need open the terminal and type
+//npm istall prompt-sync after that below program
+
+const prompt = require("prompt-sync")();
+
+const month = prompt("Enter month number :");
+
+const day =prompt("Enter Day number :");
+
+if(month==3&&day>=20 &&day<=30 || month==4&&day>=1&&day<=31||month==5&&day>=1&&day<=30||month==6&&day<=31){
+    console.log("True")
+}else{
+    console.log("False");
+}
